5047625f410a95928372d5158a5d9cd583c7c778
NerijusJurka <nerijus.jurka@gmail.com>
2023-06-10 23:44:06 +0300

    AddExpenses: This class handles the functionality for adding new expenses to the ExpenseTracker application. The AddExpense method allows the user to input the details of a new expense and stores it in the database.

---
+++ ExpenseTracker/AddExpenses.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker
+{
+    public class AddExpenses
+    {
+        public void AddExpense(User user)
+        {
+            Console.WriteLine("Enter expense description: ");
+            string description = Console.ReadLine();
+
+            Console.WriteLine("Enter expense amount: ");
+            decimal amount;
+            while (!decimal.TryParse(Console.ReadLine(), out amount))
+            {
+                Console.WriteLine("Invalid amount. Please enter a valid decimal value.");
+                Console.Write("Enter expense amount: ");
+            }
+            Console.WriteLine("Enter expense date (YYYY-MM-DD): ");
+            DateTime date;
+            while (!DateTime.TryParse(Console.ReadLine(),out date))
+            {
+                Console.WriteLine("Invalid date format. Please enter a valid date in the format YYYY-MM-DD.");
+                Console.Write("Enter expense date (YYYY-MM-DD): ");
+            }
+            Expense expense = new Expense
+            {
+                Description = description,
+                Amount = amount,
+                Date = date,
+                UserId = user.Id
+            };
+            SaveExpense(expense);
+
+            Console.WriteLine("Expense added successfully.");
+        }
+        private void SaveExpense(Expense expense)
+        {
+
+        }
+    }
+}
