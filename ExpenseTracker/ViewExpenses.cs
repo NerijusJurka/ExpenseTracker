322c5fad685d242cd0654b9d62daa81dee64207c
NerijusJurka <nerijus.jurka@gmail.com>
2023-06-10 23:44:40 +0300

    ViewExpenses: This class provides functionality for displaying expenses, typically associated with a specific user. The DisplayExpenses method can be used to retrieve and present expenses from the database for a given user.

---
+++ ExpenseTracker/ViewExpenses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker
+{
+    public class ViewExpenses
+    {
+        public void DisplayExpenses(User user)
+        {
+            List<Expense> expenses = RetrieveExpenses(user);
+
+            if (expenses.Count == 0) 
+            {
+                Console.WriteLine("No expenses found.");
+                return;
+            }
+            Console.WriteLine("Expense List");
+            Console.WriteLine("============");
+            foreach (Expense expense in expenses)
+            {
+                Console.WriteLine($"ID: {expense.Id}");
+                Console.WriteLine($"Description: {expense.Description}");
+                Console.WriteLine($"Amount: {expense.Amount:C}");
+                Console.WriteLine($"Date: {expense.Date}");
+                Console.WriteLine("-------------------");
+            }
+        }
+        private List<Expense> RetrieveExpenses(User user)
+        {
+            return new List<Expense>();
+        }
+    }
+}
