84ff8b4f898deec212ba2429e9fb1250bfa7c7ae
NerijusJurka <nerijus.jurka@gmail.com>
2023-06-10 14:21:19 +0300

    Added a method HidePasswordInput() to securely hide the password input while typing. Modified the database table creation query to include a PasswordHash column instead of Password. Implemented password hashing using SHA-256 algorithm to generate a password hash. Stored the hashed password in the PasswordHash column of the database.

--- ExpenseTracker/Registration.cs
+++ ExpenseTracker/Registration.cs
@@ -1,14 +1,19 @@
 ﻿using ExpenseTracker;
 using Microsoft.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
+
 public class Registration
 {
     public void Signup()
     {
         Console.Write("Enter username: ");
         string username = Console.ReadLine();
+
         Console.Write("Enter password: ");
-        string password = Console.ReadLine();
+        string password = HidePasswordInput();
+
         Console.Write("Enter email: ");
         string email = Console.ReadLine();
 
@@ -17,7 +22,7 @@ public class Registration
 
         string tableName = "Users";
         string queryCheckTableExists = $"SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{tableName}'";
-        string queryCreateTable = $"CREATE TABLE {tableName} (Id INT PRIMARY KEY IDENTITY, Username NVARCHAR(50) NOT NULL, Password NVARCHAR(50) NOT NULL, Email NVARCHAR(50) NOT NULL)";
+        string queryCreateTable = $"CREATE TABLE {tableName} (Id INT PRIMARY KEY IDENTITY, Username NVARCHAR(50) NOT NULL, PasswordHash NVARCHAR(64) NOT NULL, Email NVARCHAR(50) NOT NULL)";
 
         using (SqlConnection connection = new SqlConnection(connectionString))
         {
@@ -36,10 +41,19 @@ public class Registration
                         Console.WriteLine("Table created successfully.");
                     }
 
+                    // Generate password hash
+                    byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                    byte[] hashBytes;
+                    using (SHA256 sha256 = SHA256.Create())
+                    {
+                        hashBytes = sha256.ComputeHash(passwordBytes);
+                    }
+                    string passwordHash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+
                     // Insert the user data
-                    command.CommandText = "INSERT INTO Users (Username, Password, Email) VALUES (@Username, @Password, @Email); SELECT SCOPE_IDENTITY();";
+                    command.CommandText = "INSERT INTO Users (Username, PasswordHash, Email) VALUES (@Username, @PasswordHash, @Email); SELECT SCOPE_IDENTITY();";
                     command.Parameters.AddWithValue("@Username", username);
-                    command.Parameters.AddWithValue("@Password", password);
+                    command.Parameters.AddWithValue("@PasswordHash", passwordHash);
                     command.Parameters.AddWithValue("@Email", email);
                     int userId = Convert.ToInt32(command.ExecuteScalar());
                     Console.WriteLine($"User with ID {userId} registered successfully.");
@@ -54,5 +68,33 @@ public class Registration
                 }
             }
         }
+    }
+
+    private string HidePasswordInput()
+    {
+        StringBuilder password = new StringBuilder();
+        ConsoleKeyInfo key;
+
+        do
+        {
+            key = Console.ReadKey(true);
+
+            // Append asterisk (*) character to password
+            if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+            {
+                password.Append(key.KeyChar);
+                Console.Write("*");
+            }
+            else if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+            {
+                password.Remove(password.Length - 1, 1);
+                Console.Write("\b \b");
+            }
+        }
+        while (key.Key != ConsoleKey.Enter);
+
+        Console.WriteLine();
+
+        return password.ToString();
     }
 }
