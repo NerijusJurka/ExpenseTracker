4a68fdc696ec53870023e137a2eadc618a4d96cf
NerijusJurka <nerijus.jurka@gmail.com>
2023-06-10 14:21:09 +0300

    Updated the query to retrieve the PasswordHash from the database instead of Password. Implemented password hashing using SHA-256 algorithm to hash the entered password. Compared the entered password hash with the stored password hash from the database for authentication.

--- ExpenseTracker/Login.cs
+++ ExpenseTracker/Login.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ExpenseTracker
 {
@@ -9,24 +11,40 @@ namespace ExpenseTracker
             // Connection string
             string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\jurka\\source\\repos\\ExpenseTracker\\ExpenseTracker\\Database.mdf;Integrated Security=True";
 
-            string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password";
+            string query = "SELECT PasswordHash FROM Users WHERE Username = @Username";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Username", username);
-                    command.Parameters.AddWithValue("@Password", password);
 
                     try
                     {
                         connection.Open();
-                        int count = (int)command.ExecuteScalar();
+                        string passwordHashFromDb = (string)command.ExecuteScalar();
 
-                        if (count > 0)
+                        if (passwordHashFromDb != null)
                         {
-                            Console.WriteLine("Login successful!");
-                            //Another class call
+                            // Hash the entered password
+                            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                            byte[] hashBytes;
+                            using (SHA256 sha256 = SHA256.Create())
+                            {
+                                hashBytes = sha256.ComputeHash(passwordBytes);
+                            }
+                            string enteredPasswordHash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+
+                            // Compare the hashed passwords
+                            if (passwordHashFromDb.Equals(enteredPasswordHash))
+                            {
+                                Console.WriteLine("Login successful!");
+                                // Another class call
+                            }
+                            else
+                            {
+                                Console.WriteLine("Username or password is incorrect.");
+                            }
                         }
                         else
                         {
