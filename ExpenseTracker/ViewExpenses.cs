1add174d4e7752dae24ef4307f5d63db9af5e0fa
NerijusJurka <nerijus.jurka@gmail.com>
2023-06-12 23:29:40 +0300

    Added a private method RetrieveExpenses to the ViewExpenses class. This method retrieves a list of expenses from the database based on the provided user.

--- ExpenseTracker/ViewExpenses.cs
+++ ExpenseTracker/ViewExpenses.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,17 +9,27 @@ namespace ExpenseTracker
 {
     public class ViewExpenses
     {
+        private readonly string connectionString;
+
+        public ViewExpenses(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
         public void DisplayExpenses(User user)
         {
+
             List<Expense> expenses = RetrieveExpenses(user);
 
-            if (expenses.Count == 0) 
+            Console.WriteLine(user.Id);
+            if (expenses.Count == 0)
             {
                 Console.WriteLine("No expenses found.");
                 return;
             }
+
             Console.WriteLine("Expense List");
             Console.WriteLine("============");
+
             foreach (Expense expense in expenses)
             {
                 Console.WriteLine($"ID: {expense.Id}");
@@ -30,7 +41,48 @@ namespace ExpenseTracker
         }
         private List<Expense> RetrieveExpenses(User user)
         {
-            return new List<Expense>();
+            string query = "SELECT Id, Description, Amount, Date FROM Expenses WHERE UserId = @UserId";
+
+            List<Expense> expenses = new List<Expense>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UserId", user.Id);
+
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            int expenseId = (int)reader["Id"];
+                            string description = (string)reader["Description"];
+                            decimal amount = (decimal)reader["Amount"];
+                            DateTime date = (DateTime)reader["Date"];
+
+                            Expense expense = new Expense
+                            {
+                                Id = expenseId,
+                                Description = description,
+                                Amount = amount,
+                                Date = date,
+                                UserId = user.Id
+                            };
+
+                            expenses.Add(expense);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+                }
+            }
+
+            return expenses;
         }
     }
 }
