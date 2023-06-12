b7cd854889c72394d6ef26a5d9c863968e95b77d
NerijusJurka <nerijus.jurka@gmail.com>
2023-06-12 23:28:51 +0300

    Added a private method GetUserByUsername to the Login class. This method retrieves a user from the database based on the provided username. It establishes a connection to the database using a connection string and executes a SQL query to fetch the user's data.

--- ExpenseTracker/Login.cs
+++ ExpenseTracker/Login.cs
@@ -39,7 +39,17 @@ namespace ExpenseTracker
                             if (passwordHashFromDb.Equals(enteredPasswordHash))
                             {
                                 Console.WriteLine("Login successful!");
-                                // Another class call
+                                var expenseTrackerDashBoard = new ExpenseTrackerDashboard(connectionString);
+                                var user = GetUserByUsername(username);
+
+                                if (user != null)
+                                {
+                                    expenseTrackerDashBoard.DisplayDashboard(user);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Failed to fetch user data from the database.");
+                                }
                             }
                             else
                             {
@@ -57,6 +67,51 @@ namespace ExpenseTracker
                     }
                 }
             }
+        }
+        private User GetUserByUsername(string username)
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\jurka\\source\\repos\\ExpenseTracker\\ExpenseTracker\\Database.mdf;Integrated Security=True";
+
+            string query = "SELECT Id, Username, PasswordHash, Email FROM Users WHERE Username = @Username";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", username);
+
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        if (reader.Read())
+                        {
+                            int userId = (int)reader["Id"];
+                            string fetchedUsername = (string)reader["Username"];
+                            string fetchedPassword = (string)reader["PasswordHash"];
+                            string fetchedEmail = (string)reader["Email"];
+
+                            // Create a new User object and populate it with the fetched data
+                            User user = new User
+                            {
+                                Id = userId,
+                                Username = fetchedUsername,
+                                PasswordHash = fetchedPassword,
+                                Email = fetchedEmail
+                            };
+
+                            return user;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+                }
+            }
+
+            return null; // User not found
         }
     }
 }
