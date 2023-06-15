e79c642e3da28c0fe67713cfccb64ccb2ad0e9f8
NerijusJurka <nerijus.jurka@gmail.com>
2023-06-15 21:05:14 +0300

    Implement logout functionality with delay before returning to main menu

---
+++ ExpenseTracker/Logout.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker
+{
+    public class Logout
+    {
+        public void PerformLogout()
+        {
+            Console.WriteLine("Logging out...");
+            Thread.Sleep(5000);
+            Program.DisplayMainMenu();
+        }
+    }
+}
