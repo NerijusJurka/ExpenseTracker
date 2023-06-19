522cea3f0ae06b04a92085858ccc078d69e88938
NerijusJurka <nerijus.jurka@gmail.com>
2023-06-19 23:50:18 +0300

    Added FilterOptions class for specifying expense filters

---
+++ ExpenseTracker/Filters/FilterOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Filters
+{
+    public class FilterOptions
+    {
+        public bool FilterById { get; set; }
+        public int Id { get; set; }
+
+        public bool FilterByAmount { get; set; }
+        public decimal Amount { get; set; }
+
+        public bool FilterByDate { get; set; }
+        public DateTime Date { get; set; }
+
+        public bool FilterByCategory { get; set; }
+        public string Category { get; set; }
+
+        public bool FilterByPaymentMethod { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+}
