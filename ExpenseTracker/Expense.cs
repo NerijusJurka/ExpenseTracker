96f4bba0151d5e33fe34754b858b586d9954b107
NerijusJurka <nerijus.jurka@gmail.com>
2023-06-19 15:22:45 +0300

    Added a PaymentMethod property of type PaymentMethod enum to represent the payment method used for the expense. Added a Category property of type Category enum to represent the category of the expense.

--- ExpenseTracker/Expense.cs
+++ ExpenseTracker/Expense.cs
@@ -13,6 +13,8 @@ namespace ExpenseTracker
         public decimal Amount { get; set; }
         public DateTime Date { get; set; }
         public int UserId { get; set; }
+        public string Category { get; set; }
+        public string PaymentMethod { get; set; }
 
         public User User { get; set; }
     }
