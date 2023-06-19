be10afe5d7aa20d9f96e421b7f803f2db80859f9
NerijusJurka <nerijus.jurka@gmail.com>
2023-06-19 15:55:31 +0300

    Refactored the main program flow in the Program.cs class.

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -8,28 +8,26 @@ namespace ExpenseTracker
         {
             DisplayMainMenu();
         }
+
         public static void DisplayMainMenu()
         {
+            Console.Clear();
             Console.WriteLine("Expense Tracker");
+            Console.WriteLine("================");
             Console.WriteLine("1. Login");
             Console.WriteLine("2. Registration");
             Console.WriteLine("3. Exit");
 
+            Console.Write("Enter your choice: ");
             string userInput = Console.ReadLine();
 
             switch (userInput)
             {
                 case "1":
-                    Login login = new Login();
-                    Console.Write("Enter username: ");
-                    string username = Console.ReadLine();
-                    Console.Write("Enter password: ");
-                    string password = Console.ReadLine();
-                    login.SignIn(username, password);
+                    PerformLogin();
                     break;
                 case "2":
-                    Registration registration = new Registration();
-                    registration.Signup();
+                    PerformRegistration();
                     break;
                 case "3":
                     Console.WriteLine("Exiting...");
@@ -41,6 +39,31 @@ namespace ExpenseTracker
 
             Console.ReadLine();
         }
-            
+
+        private static void PerformLogin()
+        {
+            Console.Clear();
+            Console.WriteLine("Login");
+            Console.WriteLine("=====");
+
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            Login login = new Login();
+            login.SignIn(username, password);
+        }
+
+        private static void PerformRegistration()
+        {
+            Console.Clear();
+            Console.WriteLine("Registration");
+            Console.WriteLine("============");
+
+            Registration registration = new Registration();
+            registration.Signup();
+        }
     }
 }
