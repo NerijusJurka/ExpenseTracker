7ae1bb7082c990ed2ffa1d692dcb7373a913e65b
NerijusJurka <nerijus.jurka@gmail.com>
2023-07-13 20:26:50 +0300

    Refactor DatabaseAccess class as abstract base class The DatabaseAccess class has been refactored as an abstract base class to provide common functionality for database access across different derived classes. The connectionString field is now protected to allow access from derived classes. The ExecuteScalar<T> method has also been implemented to execute a scalar query with an optional SqlParameter. It establishes a connection using the provided connection string, creates a SqlCommand with the query and parameter, executes the command, and returns the result. This abstract base class serves as a foundation for database access operations and can be extended by derived classes to implement specific database operations.

---
+++ ExpenseTracker/Data/DatabaseAccess.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Data
+{
+    public abstract class DatabaseAccess
+    {
+        protected readonly string connectionString;
+
+        protected DatabaseAccess(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        protected T ExecuteScalar<T>(string query, SqlParameter parameter)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    if (parameter != null)
+                    {
+                        command.Parameters.Add(parameter);
+                    }
+
+                    try
+                    {
+                        connection.Open();
+                        return (T)command.ExecuteScalar();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+                }
+            }
+
+            return default(T);
+        }
+    }
+}
