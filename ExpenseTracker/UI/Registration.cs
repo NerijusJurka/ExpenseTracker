7a04b2b61a8b16b6fee038c1b0f0d73f7b6b8ac1
NerijusJurka <nerijus.jurka@gmail.com>
2023-07-13 20:31:41 +0300

    The updated code for the Registration class shows several improvements. The class now inherits from the DatabaseAccess base class, allowing it to utilize the common database access functionality.

--- ExpenseTracker/UI/Registration.cs
+++ ExpenseTracker/UI/Registration.cs
@@ -1,12 +1,16 @@
 ﻿using ExpenseTracker;
+using ExpenseTracker.Data;
 using Microsoft.Data.SqlClient;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 
-public class Registration
+public class Registration : DatabaseAccess
 {
+    public Registration(string connectionString) : base(connectionString)
+    {
+    }
     public void Signup()
     {
         string username, password, email;
@@ -39,65 +43,68 @@ public class Registration
             }
 
         } while (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(email));
-        
-            // Connection string
-            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\jurka\\source\\repos\\ExpenseTracker\\ExpenseTracker\\Data\\Database.mdf;Integrated Security=True";
 
-            string tableName = "Users";
-            string queryCheckTableExists = $"SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{tableName}'";
-            string queryCreateTable = $"CREATE TABLE {tableName} (Id INT PRIMARY KEY IDENTITY, Username NVARCHAR(50) NOT NULL, PasswordHash NVARCHAR(64) NOT NULL, Email NVARCHAR(50) NOT NULL)";
+        string tableName = "Users";
+        string queryCheckTableExists = $"SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{tableName}'";
+        string queryCreateTable = $"CREATE TABLE {tableName} (Id INT PRIMARY KEY IDENTITY, Username NVARCHAR(50) NOT NULL, PasswordHash NVARCHAR(64) NOT NULL, Email NVARCHAR(50) NOT NULL)";
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand command = new SqlCommand(queryCheckTableExists, connection))
             {
-                using (SqlCommand command = new SqlCommand(queryCheckTableExists, connection))
+                try
                 {
-                    try
+                    connection.Open();
+                    int tableCount = (int)command.ExecuteScalar();
+
+                    if (tableCount == 0)
                     {
-                        connection.Open();
-                        int tableCount = (int)command.ExecuteScalar();
-
-                        if (tableCount == 0)
-                        {
-                            // Table does not exist, create it
-                            command.CommandText = queryCreateTable;
-                            command.ExecuteNonQuery();
-                            Console.WriteLine("Table created successfully.");
-                        }
-
-                        // Generate password hash
-                        byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
-                        byte[] hashBytes;
-                        using (SHA256 sha256 = SHA256.Create())
-                        {
-                            hashBytes = sha256.ComputeHash(passwordBytes);
-                        }
-                        string passwordHash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
-
-                        // Insert the user data
-                        command.CommandText = "INSERT INTO Users (Username, PasswordHash, Email) VALUES (@Username, @PasswordHash, @Email); SELECT SCOPE_IDENTITY();";
-                        command.Parameters.AddWithValue("@Username", username);
-                        command.Parameters.AddWithValue("@PasswordHash", passwordHash);
-                        command.Parameters.AddWithValue("@Email", email);
-                        int userId = Convert.ToInt32(command.ExecuteScalar());
-                        Console.WriteLine($"User with ID {userId} registered successfully.");
-                        Console.WriteLine("Redirecting to the main menu...");
-                        Thread.Sleep(5000); // Delay for 5 seconds (5000 milliseconds)
-                        Console.Clear();
-                        Program.DisplayMainMenu();
+                        // Table does not exist, create it
+                        command.CommandText = queryCreateTable;
+                        command.ExecuteNonQuery();
+                        Console.WriteLine("Table created successfully.");
                     }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"Error: {ex.Message}");
-                    }
+
+                    // Generate password hash
+                    string passwordHash = GetPasswordHash(password);
+
+                    // Insert the user data
+                    command.CommandText = "INSERT INTO Users (Username, PasswordHash, Email) VALUES (@Username, @PasswordHash, @Email); SELECT SCOPE_IDENTITY();";
+                    command.Parameters.AddWithValue("@Username", username);
+                    command.Parameters.AddWithValue("@PasswordHash", passwordHash);
+                    command.Parameters.AddWithValue("@Email", email);
+                    int userId = Convert.ToInt32(command.ExecuteScalar());
+                    Console.WriteLine($"User with ID {userId} registered successfully.");
+                    Console.WriteLine("Redirecting to the main menu...");
+                    Thread.Sleep(5000); // Delay for 5 seconds (5000 milliseconds)
+                    Console.Clear();
+                    Program.DisplayMainMenu();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
                 }
             }
+        }
     }
+
+    private string GetPasswordHash(string password)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            byte[] hashBytes = sha256.ComputeHash(passwordBytes);
+            return BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+        }
+    }
+
     private bool IsValidEmail(string email)
     {
         // Email validation using regular expression
         string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
         return Regex.IsMatch(email, pattern);
     }
+
     private string HidePasswordInput()
     {
         StringBuilder password = new StringBuilder();
@@ -125,32 +132,13 @@ public class Registration
 
         return password.ToString();
     }
+
     private bool IsUsernameTaken(string username)
     {
-        // Connection string
-        string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\jurka\\source\\repos\\ExpenseTracker\\ExpenseTracker\\Data\\Database.mdf;Integrated Security=True";
-
         string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username";
+        SqlParameter parameter = new SqlParameter("@Username", username);
 
-        using (SqlConnection connection = new SqlConnection(connectionString))
-        {
-            using (SqlCommand command = new SqlCommand(query, connection))
-            {
-                command.Parameters.AddWithValue("@Username", username);
-
-                try
-                {
-                    connection.Open();
-                    int count = (int)command.ExecuteScalar();
-                    return count > 0;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error: {ex.Message}");
-                }
-            }
-        }
-
-        return false;
+        int count = ExecuteScalar<int>(query, parameter);
+        return count > 0;
     }
 }
