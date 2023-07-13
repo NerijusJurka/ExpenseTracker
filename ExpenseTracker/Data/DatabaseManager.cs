ded585f7c44d644cc470e33e6e92265626c05493
NerijusJurka <nerijus.jurka@gmail.com>
2023-07-13 20:27:40 +0300

    Implement DatabaseManager class for managing the connection string The DatabaseManager class provides a simple implementation for managing the connection string used for database operations. It takes the connection string as a parameter in the constructor and provides a GetConnectionString() method to retrieve the stored connection string. This class can be used as a central point for managing and accessing the connection string throughout the application, allowing for easy configuration and flexibility.

---
+++ ExpenseTracker/Data/DatabaseManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Data
+{
+    public class DatabaseManager
+    {
+        private readonly string connectionString;
+
+        public DatabaseManager(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public string GetConnectionString()
+        {
+            return connectionString;
+        }
+    }
+}
