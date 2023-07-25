c0160f6a5cabd9cd3cd97fcaef7a5b9d81ddf2f4
NerijusJurka <nerijus.jurka@gmail.com>
2023-07-25 23:37:55 +0300

    "Add database table creation and connection settings"

--- ExpenseTracker/Data/DatabaseManager.cs
+++ ExpenseTracker/Data/DatabaseManager.cs
@@ -1,23 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+using System.Data;
 
 namespace ExpenseTracker.Data
 {
-    public class DatabaseManager
+    public class ConnectionSettings
     {
-        private readonly string connectionString;
+        public string ConnectionString { get; set; }
+    }
 
-        public DatabaseManager(string connectionString)
+    public class DatabaseManager : IDatabaseAccess
+    {
+        private readonly IOptions<ConnectionSettings> settings;
+
+        public DatabaseManager(IOptions<ConnectionSettings> settings)
         {
-            this.connectionString = connectionString;
+            this.settings = settings;
+        }
+        public void EnsureDatabaseTableExists()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string checkTableExistsCommandText = @"
+                IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Users]') AND type in (N'U'))
+                BEGIN
+                    CREATE TABLE [dbo].[Users](
+                        [Id] [int] IDENTITY(1,1) NOT NULL,
+                        [Username] [nvarchar](50) NOT NULL,
+                        [PasswordHash] [nvarchar](128) NOT NULL,
+                        [PasswordSalt] [nvarchar](128) NOT NULL,
+                        [Email] [nvarchar](50) NOT NULL,
+                        [IsActive] [bit] NOT NULL
+                    )
+                END";
+
+                using (var command = new SqlCommand(checkTableExistsCommandText, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
         }
 
+        public string ConnectionString => settings.Value.ConnectionString;
+
+        public async Task<T> ExecuteScalarAsync<T>(string commandText, params IDataParameter[] parameters)
+        {
+            return default(T);
+        }
+
+        public async Task<List<T>> ExecuteQueryAsync<T>(string commandText, params IDataParameter[] parameters) where T : class, new()
+        {
+            return new List<T>();
+        }
+
+        public async Task<int> ExecuteNonQueryAsync(string commandText, params IDataParameter[] parameters)
+        {
+            return await Task.FromResult(0);
+        }
         public string GetConnectionString()
         {
-            return connectionString;
+            return settings.Value.ConnectionString;
         }
     }
 }
