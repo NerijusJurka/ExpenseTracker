ab7562f78b680b043f6567bd84ee7c0298c5f643
NerijusJurka <nerijus.jurka@gmail.com>
2023-07-25 23:47:51 +0300

    Updated Registration class to use dependency injection for IUserService and IPasswordHasher. Made SignUpAsync method asynchronous and refactored the signup logic accordingly.

--- ExpenseTracker/UI/Registration.cs
+++ ExpenseTracker/UI/Registration.cs
@@ -1,103 +1,81 @@
 ﻿using ExpenseTracker;
 using ExpenseTracker.Data;
+using ExpenseTracker.Model;
+using ExpenseTracker.Services;
 using Microsoft.Data.SqlClient;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 
-public class Registration : DatabaseAccess
+public class Registration
 {
-    public Registration(string connectionString) : base(connectionString)
+    private readonly IUserService userService;
+    private readonly IPasswordHasher passwordHasher;
+    private string connectionString;
+
+    public Registration(string connectionString, IUserService userService, IPasswordHasher passwordHasher)
     {
+        this.connectionString = connectionString;
+        this.userService = userService;
+        this.passwordHasher = passwordHasher;
     }
-    public void Signup()
-    {
-        string username, password, email;
 
-        do
+    public async Task SignUpAsync()
+    {   
+        try
         {
-            Console.Write("Enter username: ");
-            username = Console.ReadLine();
-
-            while (IsUsernameTaken(username))
+            string username, password, email;
+            do
             {
-                Console.WriteLine("Username is already taken. Please choose another username.");
                 Console.Write("Enter username: ");
                 username = Console.ReadLine();
-            }
 
-            Console.Write("Enter password: ");
-            password = HidePasswordInput();
+                while (await IsUsernameTakenAsync(username))
+                {
+                    Console.WriteLine("Username is already taken. Please choose another username.");
+                    Console.Write("Enter username: ");
+                    username = Console.ReadLine();
+                }
 
-            Console.Write("Enter email: ");
-            email = Console.ReadLine();
+                Console.Write("Enter password: ");
+                password = HidePasswordInput();
 
-            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(email))
-            {
-                Console.WriteLine("Username, password, and email cannot be null or empty. Please try again.");
-            }
-            else if (!IsValidEmail(email))
-            {
-                Console.WriteLine("Invalid email format. Please enter a valid email address.");
-            }
+                Console.Write("Enter email: ");
+                email = Console.ReadLine();
 
-        } while (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(email));
+                if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(email))
+                {
+                    Console.WriteLine("Username, password, and email cannot be null or empty. Please try again.");
+                }
+                else if (!IsValidEmail(email))
+                {
+                    Console.WriteLine("Invalid email format. Please enter a valid email address.");
+                }
 
-        string tableName = "Users";
-        string queryCheckTableExists = $"SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{tableName}'";
-        string queryCreateTable = $"CREATE TABLE {tableName} (Id INT PRIMARY KEY IDENTITY, Username NVARCHAR(50) NOT NULL, PasswordHash NVARCHAR(64) NOT NULL, Email NVARCHAR(50) NOT NULL)";
+            } while (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(email));
 
-        using (SqlConnection connection = new SqlConnection(connectionString))
+            // Generate password hash
+            string passwordSalt = passwordHasher.GenerateSalt();
+
+            string passwordHash = passwordHasher.HashPassword(password, passwordSalt);
+
+            User user = new User(0, username, passwordHash, passwordSalt, email, false);
+
+            await userService.CreateUserAsync(user);
+
+            Console.WriteLine("User registered successfully.");
+            Console.WriteLine("Redirecting to the main menu...");
+            Thread.Sleep(5000); // Delay for 5 seconds (5000 milliseconds)
+            Console.Clear();
+            Program.DisplayMainMenu();
+        }
+        catch(Exception ex)
         {
-            using (SqlCommand command = new SqlCommand(queryCheckTableExists, connection))
-            {
-                try
-                {
-                    connection.Open();
-                    int tableCount = (int)command.ExecuteScalar();
-
-                    if (tableCount == 0)
-                    {
-                        // Table does not exist, create it
-                        command.CommandText = queryCreateTable;
-                        command.ExecuteNonQuery();
-                        Console.WriteLine("Table created successfully.");
-                    }
-
-                    // Generate password hash
-                    string passwordHash = GetPasswordHash(password);
-
-                    // Insert the user data
-                    command.CommandText = "INSERT INTO Users (Username, PasswordHash, Email) VALUES (@Username, @PasswordHash, @Email); SELECT SCOPE_IDENTITY();";
-                    command.Parameters.AddWithValue("@Username", username);
-                    command.Parameters.AddWithValue("@PasswordHash", passwordHash);
-                    command.Parameters.AddWithValue("@Email", email);
-                    int userId = Convert.ToInt32(command.ExecuteScalar());
-                    Console.WriteLine($"User with ID {userId} registered successfully.");
-                    Console.WriteLine("Redirecting to the main menu...");
-                    Thread.Sleep(5000); // Delay for 5 seconds (5000 milliseconds)
-                    Console.Clear();
-                    Program.DisplayMainMenu();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error: {ex.Message}");
-                }
-            }
+            Console.WriteLine("An error occurred during registration: " + ex.ToString());
+            Console.ReadLine(); // Wait for the user to press Enter before closing the console
         }
     }
-
-    private string GetPasswordHash(string password)
-    {
-        using (SHA256 sha256 = SHA256.Create())
-        {
-            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
-            byte[] hashBytes = sha256.ComputeHash(passwordBytes);
-            return BitConverter.ToString(hashBytes).Replace("-", string.Empty);
-        }
-    }
-
     private bool IsValidEmail(string email)
     {
         // Email validation using regular expression
@@ -133,12 +111,9 @@ public class Registration : DatabaseAccess
         return password.ToString();
     }
 
-    private bool IsUsernameTaken(string username)
+    private async Task<bool> IsUsernameTakenAsync(string username)
     {
-        string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username";
-        SqlParameter parameter = new SqlParameter("@Username", username);
-
-        int count = ExecuteScalar<int>(query, parameter);
-        return count > 0;
+        var user = await userService.GetUserByUsernameAsync(username);
+        return user != null;
     }
 }
