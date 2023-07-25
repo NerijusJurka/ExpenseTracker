99b289f02605d4c82f70f2a688bd404414dcf477
NerijusJurka <nerijus.jurka@gmail.com>
2023-07-25 23:43:18 +0300

    Password hashing interface for secure authentication

---
+++ ExpenseTracker/Services/IPasswordHasher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Services
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password, string salt);
+        bool ValidatePassword(string enteredPassword, string storedHash, string salt);
+        string GenerateSalt();
+    }
+}
