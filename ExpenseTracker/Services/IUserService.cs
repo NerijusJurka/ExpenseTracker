0a42e2ed881c27d9821388cfc7641229dfc2a8e7
NerijusJurka <nerijus.jurka@gmail.com>
2023-07-25 23:43:34 +0300

    User service interface for managing user data and database access

---
+++ ExpenseTracker/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using ExpenseTracker.Data;
+using ExpenseTracker.Model;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Services
+{
+    public interface IUserService
+    {
+        string ConnectionString { get; }
+        Task<User> GetUserByUsernameAsync(string username);
+        string GenerateVerificationCode();
+        Task CreateUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(User user);
+        IDatabaseAccess DatabaseAccess { get; }
+    }
+}
