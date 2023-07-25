11f046c34d4de31765a2aa8669425084cd81ca18
NerijusJurka <nerijus.jurka@gmail.com>
2023-07-25 23:41:04 +0300

    "Database access interface with async methods"

---
+++ ExpenseTracker/Data/IDatabaseAccess.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ExpenseTracker.Data
+{
+    public interface IDatabaseAccess
+    {
+        string ConnectionString { get; }
+        Task<T> ExecuteScalarAsync<T>(string query, params IDataParameter[] parameters);
+        Task<int> ExecuteNonQueryAsync(string commandText, params IDataParameter[] parameters);
+        Task<List<T>> ExecuteQueryAsync<T>(string commandText, params IDataParameter[] parameters) where T : class, new();
+    }
+}
