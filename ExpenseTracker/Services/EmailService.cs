e0588faab71080909d13449f01f50dd7a16406f0
NerijusJurka <nerijus.jurka@gmail.com>
2023-07-25 23:42:49 +0300

    Email service implementation using SmtpClient

---
+++ ExpenseTracker/Services/EmailService.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Net.Mail;
+using System.Net;
+
+
+namespace ExpenseTracker.Services
+{
+    internal class EmailService : IEmailService
+    {
+        public async Task SendEmail(string to, string subject, string body)
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential("your-email@gmail.com", "your-password"),
+                EnableSsl = true
+            };
+
+            await client.SendMailAsync("your-email@gmail.com", to, subject, body);
+        }
+    }
+}
